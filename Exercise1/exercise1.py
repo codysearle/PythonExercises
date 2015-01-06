d310c28987b06423988f9820eb2db856664087cf
Cody Searle <searle.cody@gmail.com>
2015-01-06 10:09:47 -0700

    Python Exercise 1 - Coding Campus

---
+++ Exercise1/exercise1.py
@@ -0,0 +1,11 @@
+a = int(raw_input("Please enter the first integer:"))
+b = int(raw_input("Please enter the second integer:"))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+print "The sum of %s and %s is: %s" % ( a, b, c)
+print "The difference of %s and %s is: %s" % ( a, b, d)
+print "The product of %s and %s is: %s" % ( a, b, e)
+print "The quotient of %s and %s is: %s with remainder: %s" % ( a, b, f, g)
